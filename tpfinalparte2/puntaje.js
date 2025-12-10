97c9b3da41a25abe1a9b4a2ffec9707ff7d208e5
Chiara Scuderi <140880925+Melart19@users.noreply.github.com>
2025-12-10 13:37:04 -0300

    subida rec  tpfinal

---
+++ tpfinalparte2/puntaje.js
@@ -0,0 +1,16 @@
+class Puntaje {
+  constructor() {
+
+    this.valoresPelo = [3, 1, 2];      // colorado, marrón, rubio
+    this.valoresMaq = [1, 3, 2];       // marrones, azules, verdes
+    this.valoresVestido = [3, 2, 1];   // blanco, azul, rosa
+  }
+
+  calcular(pelo, maq, vestido) {
+    return (
+      this.valoresPelo[pelo - 1] +
+      this.valoresMaq[maq - 1] +
+      this.valoresVestido[vestido - 1]
+    );
+  }
+}
